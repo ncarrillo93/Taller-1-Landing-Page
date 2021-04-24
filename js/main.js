376cbd27928779bf6bc18071e859dcec789ea175
ncarrillo93 <nicolascarrillo33@gmail.com>
2021-04-24 01:35:24 -0400

    ..

--- js/main.js
+++ js/main.js
@@ -1,52 +1,26 @@
-/* OPCION 1
+
 const arrayAlerts = ["success"];
 const alerts = document.getElementById("alerts");
-
-drawAlerts();
-
-function drawAlerts(){
-    alerts.innerHTML = arrayAlerts.map((alerts,index) => alerta(alerts,index)).join('');
-}
-
-function alerta(alert,index){
-    return `<div class="alerta alert-${alert}" role="alert" onclick="deleteAlert(${index})"> 
-                ola. pero ola de saludo, no de ola. ${index}
-            </div>`;
-}
-const addAlerts = (type) => {
-    arrayAlerts.push(type);
-    drawAlerts();
-}
-function deleteAlert(index){
-    arrayAlerts.splice(index, 1);
-    drawAlerts();
-}
- */
-
-/* OPCION 2 */
-const arrayAlerts = ["success"];
-const alerts = document.getElementById("alerts");
-let comments = [document.getElementById("nombre").value
-                ,document.getElementById("comentario").value
-                ,document.getElementById("fecha").value ];
+//let comments = [document.getElementById("nombre").value,document.getElementById("comentario").value,document.getElementById("fecha").value ];
 function drawAlerts(){
   var name = document.getElementById("nombre").value;
   var com  = document.getElementById("comentario").value;
-  var date = document.getElementById("fecha").value;
+  var f = new Date();
+    var date = f.getDate() + "/" + (f.getMonth() +1) + "/" + f.getFullYear();
   let comments = [name,com,date];
+
   alerts.innerHTML = arrayAlerts.map((alert,index)=>alerta(comments,alert,index) ).join('');
 }
 
 function alerta(comments,alert,index){
-  return `<div class=" col-md-4 mb-3 mb-md-0" onclick="deleteAlert(${index})></a>
+  return `<div class="container col comments-card" onclick="deleteAlert(${index})"></a>
     <div class="card py-4 h-100">
         <div class="card-body text-center">
             <div class="d-flex justify-content-between">
-                <div>${index}</div>
                 <div class="name">${comments[0]}</div>
-                <div class="date">${comments[1]}</div>
+                <div class="date">${comments[2]}</div>
             </div>
-            <div class="comments">${comments[2]}</div>
+            <div class="comments">${comments[1]}</div>
         </div>
     </div>
 </div>`;
